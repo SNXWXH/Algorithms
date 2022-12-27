86b4487be0d4b1dfd53ed816aa5ddcbf34300c45
LSeolAh <sualah0417@gmail.com>
2022-12-27 17:37:53 +0900

    숫자 비교하기

---
+++ y22/m12/d27/#6.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+  if (num1 !== num2) answer = -1;
+  else answer = 1;
+  return answer;
+}
+
+// function solution(num1, num2) {
+//     var answer = num1 === num2 ? 1 : -1;
+//     return answer;
+// }
