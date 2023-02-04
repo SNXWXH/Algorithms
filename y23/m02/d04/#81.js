21a4f52fb3ec0c1ba0d3d368d232b533c0088143
LSeolAh <sualah0417@gmail.com>
2023-02-04 20:47:31 +0900

    숨어있는 숫자의 덧셈(2)

---
+++ y23/m02/d04/#81.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+  my_string = my_string.replace(/[^0-9]/g, " ").split(" ");
+  let answer = 0;
+  for (let i = 0; i < my_string.length; i++) answer += Number(my_string[i]);
+  return answer;
+}
