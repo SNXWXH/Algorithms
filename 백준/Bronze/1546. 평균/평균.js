b5470087a28a41b0cc22bc83320e249022803530
LSeolAh <103805620+LSeolAh@users.noreply.github.com>
2023-03-29 00:25:10 +0900

    [Bronze I] Title: 평균, Time: 120 ms, Memory: 9664 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -1,15 +1,15 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const file = process.platform === "linux" ? 0 : "./input.txt";
 
-const n = input[0];
-const score = input[1].split(' ').map(Number);
+const input = require("fs").readFileSync(file).toString().trim().split("\n");
+
+let n = input[0];
+let score = input[1].split(" ").map(Number);
 
 score.sort((a, b) => b - a);
 
-const m = score[0]; 
-const newScore = score.map(score => score/m*100);
+let m = score[0];
+let newScore = score.map((score) => (score / m) * 100);
 
 let sum = 0;
-for(let i=0; i<n; i++){
-    sum += newScore[i];
-}
-console.log(sum/n);
+for (let i = 0; i < n; i++) sum += newScore[i];
+console.log(sum / n);
