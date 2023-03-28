53d2106381e583ece77f21886bcd090bd96a8cd5
LSeolAh <103805620+LSeolAh@users.noreply.github.com>
2023-03-29 00:32:51 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let n = Number(input[0]);
+let string = input[1];
+let answer = 0;
+for(let e of string) answer += Number(e);
+console.log(answer);
