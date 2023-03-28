7b0d84573a8fc75352d17dd1d5eca0b951c1b11f
LSeolAh <snxwxh@gmail.com>
2023-03-28 16:18:34 +0900

    [1차] 비밀지도

---
+++ y23/m03/d28/Lv.1_17681.js
@@ -0,0 +1,31 @@
+function solution(n, arr1, arr2) {
+  let arr1Map = [];
+  let arr2Map = [];
+  let map = Array.from(Array(arr1.length), () => Array(n).fill(" "));
+
+  //arr1 2진수 변환 및 길이에 맞게 앞에 0 추가
+  arr1.map((e) => {
+    if (e.toString(2).length === n) arr1Map.push(e.toString(2).split(""));
+    else
+      arr1Map.push(
+        ("0".repeat(n - e.toString(2).length) + e.toString(2)).split("")
+      );
+  });
+
+  //arr1 2진수 변환 및 길이에 맞게 앞에 0 추가
+  arr2.map((e) => {
+    if (e.toString(2).length === n) arr2Map.push(e.toString(2).split(""));
+    else
+      arr2Map.push(
+        ("0".repeat(n - e.toString(2).length) + e.toString(2)).split("")
+      );
+  });
+  //두 개를 비교하면서 둘 중에 하나라고 1이라면 map에 #으로 바꾸기
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr1Map[i][j] === "1" || arr2Map[i][j] === "1") map[i][j] = "#";
+    }
+  }
+
+  return map.map((e) => e.join(""));
+}
---
+++ y23/m03/d28/README.md
@@ -0,0 +1,7 @@
+## #Lv.1 17681
+
+[https://school.programmers.co.kr/learn/courses/30/lessons/17681](https://school.programmers.co.kr/learn/courses/30/lessons/17681)
+
+처음에 2차원 배열을 이상하게 만들었는지 for문을 돌릴 때 한 번 돌아갈 때 값이 5개씩 들어가서 수정하는데 시간을 많이 썼다.
+
+arr1을 2진수로 바꾼 후 n의 길이와 맞지 않으면 앞에 0을 추가하면서 길이를 맞춰주었다.
