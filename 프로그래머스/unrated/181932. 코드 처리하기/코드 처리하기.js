f3ebb020b243b5bbb6c10c0e36682b246574d9c8
LSeolAh <103805620+LSeolAh@users.noreply.github.com>
2023-05-01 16:58:49 +0900

    [unrated] Title: 코드 처리하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,14 +1,22 @@
 function solution(code) {
-    let mode = 0
-    return [...code].reduce((acc, cur, idx) => {
-        if(mode) {
-            if(cur !== '1' && idx%2 === 1)  return acc+cur
-            if(cur === '1') mode = 0
-        } else {
-            if(cur !== '1' && idx%2 === 0)  return acc+cur
-            if(cur === '1') mode = 1
+    let mode = 0;
+    let ret = "";
+    for(let i = 0; i < code.length; i++){        
+        if(code[i] === "1"){
+            if(mode === 0)
+                mode = 1;
+            else
+                mode = 0;
+            continue;
         }
-            
-        return acc
-    }, "") || "EMPTY"
+
+        if(mode){
+            if(i%2 === 1)
+                ret += code[i];
+        }else{
+            if(i%2 === 0)
+                ret += code[i];
+        }        
+    }
+    return ret === "" ? "EMPTY" : ret;
 }
