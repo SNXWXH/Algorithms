80e1dc0ef59b093989a26a1d5f0e7a35ba0be57e
LSeolAh <103805620+LSeolAh@users.noreply.github.com>
2023-05-07 20:43:42 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,7 +1,1 @@
-function solution(num_list, n) {
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] === n)
-            return 1;
-    }
-    return 0;
-}
+const solution = (num_list, n) => num_list.includes(n) ? 1 : 0;
