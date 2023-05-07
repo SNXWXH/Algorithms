0f13dd3b546171f920d317c4e42a04c2951a23dc
LSeolAh <103805620+LSeolAh@users.noreply.github.com>
2023-05-07 21:07:32 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,7 +1,1 @@
-function solution(num_list) {
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] < 0)
-            return i;
-    }
-    return -1;
-}
+const solution = num_list => num_list.findIndex(v => v < 0);
