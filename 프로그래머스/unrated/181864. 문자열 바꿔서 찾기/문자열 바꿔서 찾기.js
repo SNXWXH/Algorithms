0b089715226dff651d19746a4de8ceda0eb11911
SeolAh Lee <103805620+LSeolAh@users.noreply.github.com>
2023-05-09 15:00:49 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,9 +1,3 @@
-function solution(myString, pat) {
-    let arr = myString.split("").map((v) => {
-        if(v === "A")
-            return "B";
-        else
-            return "A";
-    }).join("");
-    return arr.includes(pat) ? 1 : 0;
-}
+const solution = (myString, pat) => 
+[...myString].map((v) => v === 'A' ? 'B' : 'A').join('')
+.includes(pat) ? 1 : 0;
