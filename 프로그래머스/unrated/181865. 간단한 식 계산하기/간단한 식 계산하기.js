7a8e3ed7b679323a61f68a45593f80c657194eb5
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2023-05-13 22:12:15 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,9 @@
+function solution(binomial) {
+    let arr = binomial.split(" ");
+    if(arr[1] === "+")
+        return Number(arr[0]) + Number(arr[2]);
+    else if(arr[1] === "-")
+        return Number(arr[0]) - Number(arr[2]);
+    else
+        return Number(arr[0]) * Number(arr[2]);
+}
