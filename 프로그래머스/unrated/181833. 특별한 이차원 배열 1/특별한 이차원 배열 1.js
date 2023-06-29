fc017022350a23aadfb2478b476fedbc3e88a819
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2023-06-29 15:54:38 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.18 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,12 +1,8 @@
 function solution(n) {
-    let ans = Array.from(Array(n), () => Array(n).fill(null))
-    for(let i = 0; i < n; i++){
-        for(let j = 0; j < n; j++){
-            if(i != j)
-                ans[i][j] = 0;
-            else
-                ans[i][j] = 1;
-        }
-    }
-    return ans;
+  let ans = Array.from(Array(n), () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++)
+    ans[i][i] = 1;
+
+  return ans;
 }
