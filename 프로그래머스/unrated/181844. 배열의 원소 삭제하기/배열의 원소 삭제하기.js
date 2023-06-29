eed09bee324ed95e95b21ec2d0b9a0cdff86c494
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2023-06-29 16:15:49 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,9 +1,2 @@
-function solution(arr, delete_list) {
-    let ans = [...arr];
-    for(let i = 0; i < delete_list.length; i++){
-        if(ans.includes(delete_list[i])){
-            ans[ans.indexOf(delete_list[i])] = " ";
-        }
-    }
-    return ans.filter((v) => v != " ");
-}
+const solution = (arr, delete_list) => 
+    arr.filter((v) => !delete_list.includes(v));
