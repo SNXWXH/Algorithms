4dd4ba06f15814d48ad5ffc68a9ca03c447119df
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2023-07-04 15:45:13 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,1 @@
-function solution(arr, idx) {
-    for(let i = idx; i < arr.length; i++){
-        if(arr[i] === 1)
-            return i;
-    }
-    return -1;
-}
+const solution = (v,i) => v.indexOf(1, i);
