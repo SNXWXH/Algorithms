f94bc9718dbf852406a1a278bfb567f5d11a8606
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2024-02-14 11:12:33 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,22 +1,26 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let n = Number(input[0]); 
-let summary = 0;
-for (let i = 1; i <= n; i++) { 
-    let data = input[i];
-    if (check(data)) summary += 1
-}
-console.log(summary);
+const file = process.platform === 'linux' ? 0 : './input.txt';
+const [n, ...arr] = require('fs')
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split('\n');
 
-function check(data) {
-  let setData = new Set(data[0]);
-  for (let i = 0; i < data.length - 1; i++) {
-    // 오른쪽 단어와 다르다면
-    if (data[i] != data[i + 1]) {
-    // 이미 등장한 적 있는 알파벳이라면
-        if (setData.has(data[i + 1])) return false;
-        else setData.add(data[i + 1]);
-     }
+let path = '';
+let str = '';
+let sub = '';
+let count = Number(n);
+
+for (let i = 0; i < n; i++) {
+  str = arr[i];
+  for (let j = 0; j < str.length; j++) {
+    if (j === 0) continue;
+    else path = str[j - 1];
+    sub = str.slice(0, j + 1);
+    if (path !== str[j] && sub.split(str[j]).length - 1 >= 2) {
+      count--;
+      break;
+    }
   }
-  return true;
 }
+
+console.log(count);
