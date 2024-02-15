6ee7dd20b86d2b3337831ee875394116fc557a40
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2024-02-15 10:47:44 +0900

    [Silver V] Title: 단어 정렬, Time: 1296 ms, Memory: 23656 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,18 @@
+const file = process.platform === 'linux' ? 0 : './input.txt';
+const [n, ...input] = require('fs')
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split('\n');
+
+let setWord = [...new Set(input)];
+let sort = setWord.sort((a, b) => {
+  if (a.length != b.length) return a.length - b.length;
+  else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
+});
+
+sort.map((v) => console.log(v));
