03adc391285a83dd03246bdfb68cd360d48a0872
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2024-02-20 11:30:51 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 128 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,14 @@
+const file = 
+process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+const input = require("fs").readFileSync(file).toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+if (A > B)
+    console.log(">");
+else if (A < B)
+    console.log("<");
+else
+    console.log("==");
