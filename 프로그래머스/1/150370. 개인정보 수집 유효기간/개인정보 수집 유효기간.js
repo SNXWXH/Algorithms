fffab2d9d6cd102ec4349fff7ad4cdb7bdc68541
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2024-11-30 17:35:32 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.01 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,16 @@
+const solution = (today, terms, privacies) => {
+    let result = [];
+    let obj = {};
+    for(let e of terms){
+        const [type, month] = e.split(" ");
+        obj[type] = month;
+    }
+    for(let i = 0; i < privacies.length; i++){
+        const[start, type] = privacies[i].split(" ");
+        let now = new Date(start);
+        let date = new Date(today);
+        now.setMonth(now.getMonth() + Number(obj[type]));
+        if(date >= now) result.push(i+1);
+    }
+    return result;
+}
