55facfb11e8ff6257ad58f753fcbec538ae461e7
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2024-12-17 22:48:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 37.37 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -9,17 +9,14 @@ const solution = (s) => {
             if(arr[j] ==='(' || arr[j] ==='{'||arr[j] ==='[')
                 checkArr.push(arr[j]);
 
-            if(arr[j] ===')')
-                if(checkArr.pop() === '(') checkAns++;
-            if(arr[j] ==='}')
-                if(checkArr.pop() === '{') checkAns++;
-            if(arr[j] ===']')
-                if(checkArr.pop() === '[') checkAns++;
+            if(arr[j] ===')' && checkArr.pop() === '(') checkAns++;
+            if(arr[j] ==='}' && checkArr.pop() === '{') checkAns++;
+            if(arr[j] ===']' && checkArr.pop() === '[') checkAns++;
         }
+        
         if(checkAns === s.length / 2) ans++;
-        // console.log(checkArr)
-        // if(checkArr.length === 0) ans++
         checkArr = [];
+        
         let shiftLetter = arr.shift();
         arr = [...arr,shiftLetter]
     }
