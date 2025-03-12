33d35ada475be1e4b6a82a78db0a3a72e64e04db
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-03-12 17:36:45 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -0,0 +1,34 @@
+const solution = (video_len, pos, op_start, op_end, commands) => {
+    const videoTotalSec = parseTime(video_len);
+    const opStartSec = parseTime(op_start);
+    const opEndSec = parseTime(op_end);
+
+    let currentSec = parseTime(pos);
+
+    if (opStartSec <= currentSec && currentSec < opEndSec)
+        currentSec = opEndSec;
+
+    for (const command of commands) {
+        if (command === 'prev')
+            currentSec = Math.max(0, currentSec - 10);
+        else if (command === 'next')
+            currentSec = Math.min(videoTotalSec, currentSec + 10);
+
+        if (opStartSec <= currentSec && currentSec < opEndSec)
+            currentSec = opEndSec;
+    }
+
+    return formatTime(currentSec);
+};
+
+const parseTime = (time) => {
+    const [min, sec] = time.split(':').map(Number);
+    return min * 60 + sec;
+};
+
+const formatTime = (time) => {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+    return `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}`;
+};
+
