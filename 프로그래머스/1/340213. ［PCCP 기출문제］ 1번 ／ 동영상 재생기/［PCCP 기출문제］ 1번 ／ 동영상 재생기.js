65194a6a418a8cdf8d00ff7a13500f0a0afb906d
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-03-12 17:36:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,34 +1,42 @@
 const solution = (video_len, pos, op_start, op_end, commands) => {
-    const videoTotalSec = parseTime(video_len);
-    const opStartSec = parseTime(op_start);
-    const opEndSec = parseTime(op_end);
-
-    let currentSec = parseTime(pos);
-
-    if (opStartSec <= currentSec && currentSec < opEndSec)
-        currentSec = opEndSec;
-
-    for (const command of commands) {
-        if (command === 'prev')
-            currentSec = Math.max(0, currentSec - 10);
-        else if (command === 'next')
-            currentSec = Math.min(videoTotalSec, currentSec + 10);
-
-        if (opStartSec <= currentSec && currentSec < opEndSec)
-            currentSec = opEndSec;
-    }
-
-    return formatTime(currentSec);
-};
-
-const parseTime = (time) => {
-    const [min, sec] = time.split(':').map(Number);
-    return min * 60 + sec;
-};
-
-const formatTime = (time) => {
-    const min = Math.floor(time / 60);
-    const sec = time % 60;
-    return `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}`;
-};
-
+    let [min,sec] = pos.split(':').map((v)=>Number(v));
+    let [video_min,video_sec] = video_len.split(':').map((v)=>Number(v));
+    let [end_min, end_sec] = op_end.split(":").map((v)=>Number(v));
+    let [start_min, start_sec] = op_start.split(":").map((v)=>Number(v));
+    
+    
+    console.log(min,sec)
+    
+    commands.forEach((v) => {
+        if(start_min <= min && start_sec <= sec ||end_min >= min && end_sec >= sec) {
+            min = end_min;
+            sec = end_sec;
+        }
+        
+        if(v === 'prev') {
+            sec -= 10;
+            console.log(v, sec)
+        }
+        if(v === 'next') {
+            sec += 10;
+            console.log(v, sec)}
+    
+        if(sec < 0){
+            min = 0;
+            sec = 0;
+        }
+        
+        if(video_min <= min && video_sec < sec){
+            min = end_min;
+            sec = end_sec
+        }
+        
+        if(sec >= 60){
+            min += Math.floor(min / 60);
+            sec = min % 60;
+        }
+    })
+    if(min.length === 2) min = "0"+min.toString();
+    if(sec.length === 2) sec = "0"+sec.toString();
+    console.log(min,sec)
+}
