e2325431f9d7312496cc337b290e650f626e9aaa
SNXWXH <snxwxh@gmail.com>
2025-04-28 14:36:25 +0900

    [level 2] Title: 의상, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,14 @@
 const solution = (clothes) => {
-    let closet = {};
-    let ans = 1;
-    
-    clothes.forEach((v) => {
-        const [name, type] = v
-        if(!closet[type]) closet[type] = []
-        closet[type].push(name)
-    })
-    
-    let keyArray = Object.keys(closet);
-    Object.keys(closet).forEach((key) => ans *= (closet[key].length+1))
-    
-    return ans-1
-}
+  let closet = {};
+  let ans = 1;
+
+  clothes.forEach((v) => {
+    const [name, type] = v;
+    if (!closet[type]) closet[type] = [];
+    closet[type].push(name);
+  });
+
+  Object.keys(closet).forEach((key) => (ans *= closet[key].length + 1));
+
+  return ans - 1;
+};
