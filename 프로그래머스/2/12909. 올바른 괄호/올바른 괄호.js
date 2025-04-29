6d00b4703dea57af19dc8b7041c714fa82f5b61e
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-04-29 12:25:35 +0700

    [level 2] Title: 올바른 괄호, Time: 5.14 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let stack = [];
+    if (s[0] === ')') return false;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') stack.push('(');
+        else {
+            if (stack.length === 0) return false;
+            stack.pop();
+        }
+    }
+    
+    return stack.length === 0;
+}
