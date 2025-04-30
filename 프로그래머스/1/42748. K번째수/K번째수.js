e05c6b77d629989b2f4dca912caf952e1086594a
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-04-30 12:10:48 +0700

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,1 @@
-const solution = (array, commands) =>{
-    let ans = [];
-    for(commandsArr of commands){
-        const [i, j, k] = commandsArr;
-        let sliceArr = array.slice(i-1, j).sort((a,b) => a - b);
-        ans.push(sliceArr[k-1]);
-    }
-    return ans;
-}
+const solution = (array, commands) => commands.map(([i,j,k]) => array.slice(i-1,j).sort((a,b) => a-b)[k-1])
