4fe6fa774e7754b5d37f602b605ee9a145968d73
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-05-12 12:46:02 +0700

    [level 1] Title: 달리기 경주, Time: 205.45 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,14 @@
+function solution(players, callings) {
+    let obj = {};
+    players.forEach((v, idx) => obj[v] = idx);
+    for(let i = 0; i < callings.length; i++){
+        let idx = obj[callings[i]];
+        let player = players[idx-1];
+        players[idx-1] = callings[i];
+        players[idx] = player;
+        obj[callings[i]]--;
+        obj[player]++;
+    }
+    
+    return players;
+}
