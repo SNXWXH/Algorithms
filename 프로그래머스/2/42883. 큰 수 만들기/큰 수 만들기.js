cd78d37ab96ff11c18ba7a1f0257f2f05c92d93e
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-05-13 12:10:44 +0700

    [level 2] Title: 큰 수 만들기, Time: 37.86 ms, Memory: 49.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,16 @@
+function solution(number, k) {
+    const stack = []
+    let ans =''
+    
+    for(let i = 0; i < number.length; i++){
+        while(k > 0 && stack[stack.length-1] < number[i]){
+            stack.pop()
+            k--
+        }
+        stack.push(number[i])
+    }
+    
+    stack.splice(stack.length-k, k)
+    return stack.join("")
+    
+}
