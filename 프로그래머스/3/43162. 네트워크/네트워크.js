4aaaca4cad49d1dcbcc7322cea56d7e7a93997e6
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-05-26 12:05:58 +0700

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,20 +2,18 @@ const solution = (n, computers) => {
     let ans = 0;
     // 방문 여부 저장 배열
     let visited = Array(n).fill(0);
-    console.log(visited)
     
     const dfs = (i)=>{
         visited[i] = 1;
         
         // 연결된 컴퓨터가 있고, 그 컴퓨터를 방문하지 않았다면 그 컴퓨터로 dfs 실행
         for(let j = 0; j < n; j++){
-            if(computers[i][j]===1 && !visited[j]) dfs(j)
+            if(computers[i][j] === 1 && !visited[j]) dfs(j)
         }
     }
     
     // 방문하지 않았으면 dfs 호출 후 ans 1 증가
     for(let i = 0; i < n; i++){
-        console.log(visited)
         if(!visited[i]){
             dfs(i);
             ans++;
