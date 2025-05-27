16382388e03d089737297b91fea23e8c1904653a
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-05-27 12:26:40 +0700

    [level 1] Title: 유연근무제, Time: 1.72 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,20 +1,3 @@
-const solution = (schedules, timelogs, startday) => {
-    let day = ['', [6,7], [5,6], [4,5], [3,4], [2,3], [1,2], [1,7]];
-    let ans = 0;
-    
-    timelogs.map((arr,idx) => {
-        let time = timeCal(+schedules[idx]);
-        let check = 0;
-        arr.map((v,i) => {
-            if(+arr[i] <= time && !day[startday].includes(i+1)) 
-                check++;
-        })
-        if(check === arr.length-2) ans++;
-    })
-    
-    return ans;
-}
-
 const timeCal = (time) => {
     let hour = Math.floor(time / 100);
     let min = time % 100 + 10;
@@ -24,5 +7,21 @@ const timeCal = (time) => {
         min = min % 60;
     }
     
-    return hour*100 + min
+    return hour * 100 + min
+}
+
+const solution = (schedules, timelogs, startday) => {
+    let day = ['', [6,7], [5,6], [4,5], [3,4], [2,3], [1,2], [1,7]];
+    let ans = 0;
+    
+    timelogs.map((arr,idx) => {
+        let time = timeCal(+schedules[idx]);
+        let check = 0;
+        arr.map((v,i) => {
+            if(+arr[i] <= time && !day[startday].includes(i+1)) check++;
+        })
+        if(check === arr.length-2) ans++;
+    })
+    
+    return ans;
 }
