bc9a5aa7a727178c8affbb236fed135fa71b9a32
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-07-14 17:33:49 +0900

    [level 1] Title: 기사단원의 무기, Time: 121.82 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,18 +1,17 @@
 function solution(number, limit, power) {
-    let arr = []
-    let ans = 0
-    
-    for(let i = 1; i <= number; i++){
-        let idx = 1
-        let num = 1
-        
-        while (idx <= i / 2) {
-            if (i % idx === 0) num++
-            idx++
+    let ans = 0;
+
+    for (let i = 1; i <= number; i++) {
+        let count = 0;
+
+        for (let j = 1; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                count++;
+                if (j !== i / j) count++;
+            }
         }
-        arr.push(num)
+        ans += (count > limit) ? power : count;
     }
 
-    arr.map((v) => v > limit ? ans += power : ans += v)
-    return ans
+    return ans;
 }
