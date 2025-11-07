b54c65893995f46a9f9d860908d6d48903c654f7
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-11-07 11:04:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,28 +1,19 @@
-const file = 
-process.platform === "linux" ? 0 : "./input.txt";
+const file = process.platform === 'linux' ? 0 : './input.txt';
+const array = require('fs')
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
 
-const input = require("fs").readFileSync(file).toString().trim().split(" ");
+const SET = [...new Set(array)];
 
-let a = Number(input[0]);
-let b = Number(input[1]);
-let c = Number(input[2]);
-let money;
+if (SET.length === 3) console.log(Math.max(...array) * 100);
+if (SET.length === 1) console.log(10000 + array[0] * 1000);
 
-if ((a==b)&&(b==c)) {
-    money = 10000 + (a*1000);
- }
- else if ((a==b) || (b==c) || (a==c)) {
-    if (a==b)
-       money = 1000 + (a*100);
-    else if (b==c)
-       money = 1000 + (b*100);
-    else
-        money = 1000 + (c*100);
- }
- else {
-    let max = a;
-    if(max<b) max = b;
-    if(max<c) max = c;
-    money = max*100;
- }
-console.log(money);
+if (SET.length === 2) {
+  let count = array.filter((e) => e === SET[0]);
+
+  if (count.length === 2) console.log(1000 + SET[0] * 100);
+  if (count.length === 1) console.log(1000 + SET[1] * 100);
+}
