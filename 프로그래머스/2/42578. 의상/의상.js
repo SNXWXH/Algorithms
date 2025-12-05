6cb983d8796bd2b9110a40c4e20a2769b944f26d
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-12-05 19:44:09 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,14 @@
-const solution = (clothes) => {
-  let closet = {};
-  let ans = 1;
-
-  clothes.forEach((v) => {
-    const [name, type] = v;
-    if (!closet[type]) closet[type] = [];
-    closet[type].push(name);
-  });
-
-  Object.keys(closet).forEach((key) => (ans *= closet[key].length + 1));
-
-  return ans - 1;
-};
+function solution(clothes) {
+    let obj = {}
+    let ans = 1;
+    
+    clothes.forEach((v) => {
+        const [name, type] = v;
+        if (!obj[type]) obj[type] = [];
+        obj[type].push(name);
+    });
+    
+    Object.keys(obj).forEach((key) => (ans *= obj[key].length + 1));
+    
+    return ans -1
+}
