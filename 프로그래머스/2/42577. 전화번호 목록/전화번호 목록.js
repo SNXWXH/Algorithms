64b5129fe7e467944b65c83c92fc65f4bca6aa06
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-12-05 19:08:00 +0900

    [level 2] Title: 전화번호 목록, Time: 170.44 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,11 @@
-const solution = (phone_book) => {
-    phone_book.sort();
-
-    for (let i = 0; i < phone_book.length - 1; i++) {
-        if (phone_book[i+1].startsWith(phone_book[i])) return false;
+function solution(phone_book) {
+    // 반례: 	["1134", "113", "112"] -> false
+    
+    const phone_book_sort = [...phone_book].sort()
+    
+    for(let i = 0; i < phone_book.length - 1; i++){
+        if(phone_book_sort[i+1].startsWith(phone_book_sort[i])) return false         
     }
-
+    
     return true;
 }
