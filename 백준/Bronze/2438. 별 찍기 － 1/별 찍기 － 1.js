90a357a43dbdc0058a8fa1a07fed4749e465a2a8
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-12-10 10:04:50 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -1,15 +1,13 @@
-const file =
-process.platform === "linux" ? 0 : "./input.txt";
+const file = process.platform === 'linux' ? 0 : './input.txt';
+const count = require('fs').readFileSync(file).toString().trim();
 
-const input = require("fs").readFileSync(file).toString().trim();
+const star = '*';
+let ans = '';
 
-let count = Number(input);
-let star = "*";
-let answer = "";
-
-for(i=1;i<=count;i++){
-    for(j=1;j<=i;j++)
-        answer += star
-    console.log(answer);
-    answer = "";
+for (let i = 1; i <= count; i++) {
+  for (j = 1; j <= i; j++) {
+    ans += star;
+  }
+  console.log(ans);
+  ans = '';
 }
