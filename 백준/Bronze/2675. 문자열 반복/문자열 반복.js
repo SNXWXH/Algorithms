05b5c674d4ed2ad902f3caa1820b59e73f41a5ff
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-12-15 10:56:50 +0900

    [Bronze II] Title: 문자열 반복, Time: 96 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -1,14 +1,9 @@
 const file = process.platform === 'linux' ? 0 : './input.txt';
 const input = require('fs').readFileSync(file).toString().trim().split('\n');
 
-let ans = [];
-
 for (let i = 1; i < input.length; i++) {
   const [num, str] = input[i].split(' ');
-  let P = '';
-  const splitStr = str.split('');
-  splitStr.forEach((v) => (P += v.repeat(+num)));
-  ans.push(P);
-}
 
-console.log(ans.join('\n'));
+  const P = str.split('').reduce((acc, cur) => acc + cur.repeat(+num), '');
+  console.log(P);
+}
