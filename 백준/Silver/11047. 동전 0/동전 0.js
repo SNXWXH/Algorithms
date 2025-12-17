0babdaa39ed20185fbbdfbdb362fa3343f56e040
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2025-12-17 11:03:52 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -4,8 +4,10 @@ const input = require('fs').readFileSync(file).toString().trim().split('\n');
 let [, K] = input.shift().split(' ').map(Number);
 const coins = input.map(Number).sort((a, b) => b - a);
 let cnt = 0;
+
 for (const coin of coins) {
   cnt += Math.floor(K / coin);
   K %= coin;
 }
+
 console.log(cnt);
