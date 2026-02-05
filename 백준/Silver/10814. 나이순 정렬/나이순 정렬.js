f9cac8f6a60cd0f8fe7911fffe69e08d5b4dd3d6
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2026-02-05 13:54:08 +0900

    [Silver V] Title: 나이순 정렬, Time: 360 ms, Memory: 52928 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -7,10 +7,7 @@ const [N, ...INPUT] = require('fs')
 
 const USER = INPUT.map((v) => v.split(' '));
 
-const SORT_USER = USER.sort((userA, userB) => {
-  if (userA[0] === userB[0]) return userA[0] - userB[0];
-  return userA[0] - userB[0];
-});
+const SORT_USER = USER.sort((userA, userB) => userA[0] - userB[0]);
 
 const ANS = SORT_USER.map((v) => v.join(' '));
 console.log(ANS.join('\n'));
