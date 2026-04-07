f2454c065bc6a94d4046ee33b40a1402a4c6927c
SeolAh Lee <103805620+SNXWXH@users.noreply.github.com>
2026-04-07 14:47:55 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,14 +1,15 @@
-const file = process.platform === 'linux' ? 0 : './input.txt';
-const input = require('fs').readFileSync(file).toString().trim().split('-');
+const file = process.platform === 'linux' ? 0 : './INPUT.txt';
+const MATH = require('fs').readFileSync(file).toString().trim().split('-');
 
-let base = input[0].split('+').map(Number);
-let cul = base.reduce((acc, cur) => acc + cur);
-for (let i = 1; i < input.length; i++) {
-  let basic = input[i]
-    .split('+')
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
-  cul -= basic;
-}
+let ans = [];
 
-console.log(cul);
+MATH.forEach((item) => {
+  if (item.includes('+')) {
+    let sum = 0;
+    item = item.split('+').map(Number);
+    item.forEach((i) => (sum += i));
+    ans.push(sum);
+  } else ans.push(+item);
+});
+
+console.log(ans.reduce((prev, cur) => prev - cur));
